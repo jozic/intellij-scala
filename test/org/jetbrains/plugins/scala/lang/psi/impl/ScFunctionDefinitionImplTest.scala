5e99d18ff340c6aa769028a735a4009cb2309173
jozic <jozic@ua.fm>
2012-08-04 17:06:58 -0400

    simple performance tests

--- test/org/jetbrains/plugins/scala/lang/psi/impl/ScFunctionDefinitionImplTest.scala
+++ test/org/jetbrains/plugins/scala/lang/psi/impl/ScFunctionDefinitionImplTest.scala
@@ -6,6 +6,7 @@ import org.intellij.lang.annotations.Language
 import org.junit.Assert._
 import org.jetbrains.plugins.scala.lang.psi.api.statements.RecursionType.{OrdinaryRecursion, TailRecursion, NoRecursion}
 import lang.psi.api.statements.{ScFunctionDefinition, RecursionType}
+import collection.immutable.IndexedSeq
 
 /**
  * Pavel Fatin
@@ -46,5 +47,50 @@ class ScFunctionDefinitionImplTest extends SimpleTestCase {
   private def assertRecursionTypeIs(@Language("Scala") code: String, expectation: RecursionType) {
     val function = code.parse[ScFunctionDefinition]
     assertEquals(expectation, function.recursionType)
+  }
+
+  def testMyRefactorings() {
+    import lang.psi.impl.statements.ScFunctionDefinitionImpl
+    val code = toCode("""
+            def f(n: Int)(x:Int)(y:Int): Int = {
+              if (n == 34) 34
+              else if (n == 45) 45
+              else f(n + 1)(x)(y)
+            }
+                      """)
+
+    val definition = code.parse[ScFunctionDefinition].asInstanceOf[ScFunctionDefinitionImpl]
+
+    val n = 10000
+
+    def printMedianAndValue[A](name: String)(body: => A) {
+      val results: (Long, A) = calculateMedianTime(n) {
+        body
+      }
+      println(name + ": " + results._1)
+      println(name + ": " + results._2)
+      println()
+    }
+
+    printMedianAndValue("recursionTypeNew") {
+      definition.recursionType
+    }
+    printMedianAndValue("bodyNew"){
+      definition.body
+    }
+     printMedianAndValue("hasAssignNew"){
+      definition.hasAssign
+    }
+  }
+
+  def calculateMedianTime[A](n: Int)(body: => A) = {
+    val seq: IndexedSeq[(Long, A)] = for {i <- 1 to n} yield withTimer(body)
+    (seq.map(_._1).sum / n, seq.head._2)
+  }
+
+  def withTimer[A](body: => A): (Long, A) = {
+    val start: Long = System.nanoTime()
+    val res = body
+    (System.nanoTime() - start, res)
   }
 }
